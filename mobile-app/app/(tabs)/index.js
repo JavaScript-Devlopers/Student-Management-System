de34e9da24afd0708eca140f89901d1a92e70513
snehpnp <snehpnp@gmail.com>
2025-02-07 13:29:41 +0530

    Add Education and Skills components, update app icon, and modify TypeScript configuration

---
+++ mobile-app/app/(tabs)/index.js
@@ -0,0 +1,185 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity, Image, Linking } from "react-native";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+import Projects from "@/components/Projects";
+import Education from "@/components/Education";
+import Skills from "@/components/Skills";
+
+const App = () => {
+  const [isEnabled, setIsEnabled] = useState(true);
+  const [cardStyle, setCardStyle] = useState({
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: isEnabled ? "black" : "#fff",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  });
+
+  useEffect(() => {
+    setCardStyle({
+      alignItems: "center",
+      padding: 20,
+      backgroundColor: isEnabled ? "black" : "#fff",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5,
+    });
+  }, [isEnabled]);
+
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
+  const textStyle = isEnabled ? { color: "white" } : { color: "black" };
+
+  const handleLinkPress = (url) => {
+    Linking.openURL(url).catch((err) =>
+      console.error("Failed to open URL", err)
+    );
+  };
+
+  return (
+    <View style={{ backgroundColor: isEnabled ? "black" : "white", flex: 1 }}>
+      <ParallaxScrollView
+        headerBackgroundColor={{
+          bgColor: isEnabled ? "Black" : "White",
+        }}
+        headerImage={
+          <Image
+            source={require("@/assets/images/bg.png")}
+            style={{ width: "100%", height: 200, borderRadius: 10 }}
+          />
+        }
+        style={{ backgroundColor: isEnabled ? "black" : "white" }}
+      >
+        <Text
+          onPress={toggleSwitch}
+          style={{
+            alignItems: "end",
+            color: isEnabled ? "white" : "black",
+            justifyContent: "flex-end",
+            fontSize: 16,
+            marginBottom: 10,
+            textAlign: "right",
+          }}
+        >
+          <Text style={{ color: isEnabled ? "White" : "black" }}>
+            {isEnabled ? "🌙 DARK" : "💡 LIGHT"}
+          </Text>
+        </Text>
+
+        <ThemedView style={cardStyle}>
+          <Image
+            source={require("@/assets/images/profile.png")}
+            style={{ width: 120, height: 120, borderRadius: 60 }}
+          />
+          <ThemedText style={textStyle} type="title">
+            Sneh Jaiswal
+          </ThemedText>
+          <ThemedText style={textStyle} type="subtitle">
+            Full Stack Developer
+          </ThemedText>
+          <ThemedText style={textStyle}>
+            {" "}
+            <Text>📞 +91 7049510697 | 📧 snehjaiswal20@gmail.com</Text>
+          </ThemedText>
+          <Text style={{ fontSize: 16, color: "#000" }}>
+            <TouchableOpacity
+              onPress={() => handleLinkPress("https://github.com/snehpnp")}
+            >
+              <Text
+                style={{ color: "#007BFF", textDecorationLine: "underline" }}
+              >
+                🌐 GitHub
+              </Text>
+            </TouchableOpacity>
+            {" | "}
+            <TouchableOpacity
+              onPress={() =>
+                handleLinkPress(
+                  "https://www.linkedin.com/in/sneh-jaiswal-431165229/"
+                )
+              }
+            >
+              <Text
+                style={{ color: "#007BFF", textDecorationLine: "underline" }}
+              >
+                🔗 LinkedIn
+              </Text>
+            </TouchableOpacity>
+          </Text>
+        </ThemedView>
+
+        {/* About Me Section */}
+        <ThemedView
+          style={cardStyle}
+        >
+          <ThemedText style={textStyle} type="subtitle">
+            📖 About Me
+          </ThemedText>
+          <ThemedText style={textStyle}>
+            Passionate developer with expertise in React, React Native, Node.js,
+            and MongoDB. Experienced in building scalable web and mobile
+            applications.
+          </ThemedText>
+        </ThemedView>
+
+        {/* Experience Section */}
+        <ThemedView
+          style={cardStyle}
+        >
+          <ThemedText style={textStyle} type="subtitle">
+            💼 Experience
+          </ThemedText>
+      
+          <ThemedText style={[textStyle, { textAlign: "center",fontFamily: "Roboto-Bold",fontSize: 20 }]}>
+            PNP Infotech, Indore | September 2022 - Present
+          </ThemedText>
+          <ThemedText style={textStyle}>
+            - Executed the rollout of over eight major projects with a strong
+            focus on building scalable enterprise solutions. - Collaborated with
+            cross-functional teams to design and implement real-time systems. -
+            Integrated advanced features like dynamic pricing tools, real-time
+            notifications, and automated reminders. - Led the development of key
+            projects like Stock Box, Outbook, and Smart Algo. - Ensured seamless
+            integration of 30+ brokers for trading platforms. - Optimized
+            Back-End systems to handle high-volume data processing with zero
+            downtime.
+          </ThemedText>
+        </ThemedView>
+
+        {/* Featured Projects Section */}
+        <Projects isEnabled={isEnabled} cardStyle={cardStyle}/>
+
+        {/* Education Section */}
+        <Education isEnabled={isEnabled} cardStyle={cardStyle}/>
+
+        {/* Skills Section */}
+     <Skills isEnabled={isEnabled} cardStyle={cardStyle}/>
+
+
+
+        {/* Footer Section */}
+        <ThemedView style={cardStyle}>
+          <Text style={[textStyle, { textAlign: "center", margin: 20 }]}>
+            © 2025 Sneh Jaiswal. All Rights Reserved.
+          </Text>
+        </ThemedView>
+      </ParallaxScrollView>
+    </View>
+  );
+};
+
+export default App;
--- mobile-app/components/ParallaxScrollView.tsx
+++ mobile-app/components/ParallaxScrollView.tsx
@@ -1,21 +1,20 @@
-import type { PropsWithChildren, ReactElement } from 'react';
-import { StyleSheet } from 'react-native';
+import type { PropsWithChildren, ReactElement } from "react";
 import Animated, {
   interpolate,
   useAnimatedRef,
   useAnimatedStyle,
   useScrollViewOffset,
-} from 'react-native-reanimated';
+} from "react-native-reanimated";
 
-import { ThemedView } from '@/components/ThemedView';
-import { useBottomTabOverflow } from '@/components/ui/TabBarBackground';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { ThemedView } from "@/components/ThemedView";
+import { useBottomTabOverflow } from "@/components/ui/TabBarBackground";
+
 
 const HEADER_HEIGHT = 250;
 
 type Props = PropsWithChildren<{
   headerImage: ReactElement;
-  headerBackgroundColor: { dark: string; light: string };
+  headerBackgroundColor: { bgColor: string };
 }>;
 
 export default function ParallaxScrollView({
@@ -23,11 +22,13 @@ export default function ParallaxScrollView({
   headerImage,
   headerBackgroundColor,
 }: Props) {
-  const colorScheme = useColorScheme() ?? 'light';
+
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
   const scrollOffset = useScrollViewOffset(scrollRef);
   const bottom = useBottomTabOverflow();
   const headerAnimatedStyle = useAnimatedStyle(() => {
+  
+
     return {
       transform: [
         {
@@ -38,45 +39,59 @@ export default function ParallaxScrollView({
           ),
         },
         {
-          scale: interpolate(scrollOffset.value, [-HEADER_HEIGHT, 0, HEADER_HEIGHT], [2, 1, 1]),
+          scale: interpolate(
+            scrollOffset.value,
+            [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
+            [2, 1, 1]
+          ),
         },
       ],
     };
   });
 
   return (
-    <ThemedView style={styles.container}>
+    <ThemedView
+      style={{
+        flex: 1,
+        backgroundColor: headerBackgroundColor.bgColor,
+      }}
+    >
       <Animated.ScrollView
         ref={scrollRef}
         scrollEventThrottle={16}
         scrollIndicatorInsets={{ bottom }}
-        contentContainerStyle={{ paddingBottom: bottom }}>
+        contentContainerStyle={{ paddingBottom: bottom }}
+        style={{ flex: 1 , backgroundColor: headerBackgroundColor.bgColor}}
+      >
+       
         <Animated.View
           style={[
-            styles.header,
-            { backgroundColor: headerBackgroundColor[colorScheme] },
+            {
+              height: HEADER_HEIGHT,
+              overflow: "hidden",
+              backgroundColor: headerBackgroundColor.bgColor,
+            },
             headerAnimatedStyle,
-          ]}>
+          ]}
+        >
           {headerImage}
         </Animated.View>
-        <ThemedView style={styles.content}>{children}</ThemedView>
+      
+        <ThemedView
+          style={{
+            flex: 1,
+            padding: 32,
+            gap: 16,
+            overflow: "hidden",
+            borderTopLeftRadius: 24,
+            borderTopRightRadius: 24,
+            backgroundColor: headerBackgroundColor.bgColor,
+          }}
+        >
+          {children}
+        </ThemedView>
+
       </Animated.ScrollView>
     </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  header: {
-    height: HEADER_HEIGHT,
-    overflow: 'hidden',
-  },
-  content: {
-    flex: 1,
-    padding: 32,
-    gap: 16,
-    overflow: 'hidden',
-  },
-});
