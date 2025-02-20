bd5c4c6bbd9906529682073fa521929bdc3f8ae4
ajaypatidar90 <ajay.patidar90989@gmail.com>
2025-02-21 00:33:13 +0530

    done

--- backend/App/Routes/Admin/Student.routes.js
+++ backend/App/Routes/Admin/Student.routes.js
@@ -1,11 +1,12 @@
 "use strict"
 const router = require("express").Router()
-const { getAllStudent, updateStudent, addStudent } = require('../../Controllers/Admins/Student.controller')
+const { getAllStudent, updateStudent, addStudent, deleteStudent } = require('../../Controllers/Admins/Student.controller')
 
 
 router.post('/addStudent', addStudent)
 router.post('/updateStudent', updateStudent)
 router.post('/getAllStudent', getAllStudent)
+router.post('/deleteStudent', deleteStudent)
 
 
 
